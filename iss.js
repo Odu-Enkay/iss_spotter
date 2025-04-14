03ea97bc3bc6e1e5c7348d2ea920799a487f0afb
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-14 05:32:28 -0600

    impleents fetchISSFlyOverTimes

--- iss.js
+++ iss.js
@@ -42,6 +42,27 @@ callback(null, {latitude, longitude});
 });
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = body.response;
+    callback(null, passes);
+  });
+};
+
+// Don't need to export the other functions since we are not testing them right now.
+//module.exports = { };
 //module.exports = fetchMyIP;
 //module.exports = fetchCoordsByIP;
-module.exports = { fetchCoordsByIP, fetchMyIP };
+module.exports = { fetchCoordsByIP, fetchMyIP, fetchISSFlyOverTimes  };
